785e552cc06f5201c3e2ea97246f8f62d3427b78
mansijmaharzn <mansijmaharzn@gmail.com>
2024-06-13 17:53:20 +0545

    add related bikes section

--- motorcycle_haru/bikes/views.py
+++ motorcycle_haru/bikes/views.py
@@ -10,8 +10,8 @@ from .forms import NewBikeForm
 class DetailView(View):
     def get(self, request, pk):
         bike = get_object_or_404(Bike, pk=pk)
-
         related_bikes = Bike.objects.filter(category=bike.category).exclude(pk=pk)[:3]
+        
         return render(request, "bikes/detail.html", {
             'bike': bike,
             'related_bikes': related_bikes
