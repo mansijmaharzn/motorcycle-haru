941d136843a3284b466828692a37254800052074
mansijmaharzn <mansijmaharzn@gmail.com>
2024-06-20 11:15:56 +0545

    add show more info about bike in admin view

--- motorcycle_haru/bikes/admin.py
+++ motorcycle_haru/bikes/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Category, Brand, Bike
 
 
+class BikeAdmin(admin.ModelAdmin):
+    list_display= ('name', 'brand', 'category', 'owner')
+
+
 admin.site.register(Category)
 admin.site.register(Brand)
-admin.site.register(Bike)
+admin.site.register(Bike, BikeAdmin)
+
+#admin fields for data showing in admin panel
