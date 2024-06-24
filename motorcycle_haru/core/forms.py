3de181eee2da7b013ef4df4343092dbe6f5010b4
mansijmaharzn <mansijmaharzn@gmail.com>
2024-06-24 16:27:27 +0545

    add custom form validators

--- motorcycle_haru/core/forms.py
+++ motorcycle_haru/core/forms.py
@@ -1,6 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.core import validators
+
+
+def password_validator(password):
+    if len(password) < 8:
+        raise forms.ValidationError('Password must be at least 8 characters long.')
+
+    elif password.isdigit():
+        raise forms.ValidationError('Password must contain at least one letter.')
+    
+    elif password.isalpha():
+        raise forms.ValidationError('Password must contain at least one number.')
+
+    elif password.islower():
+        raise forms.ValidationError('Password must contain at least one uppercase letter.')
+    
+    elif password.isupper():
+        raise forms.ValidationError('Password must contain at least one lowercase letter.')
+    
+    elif password.isalnum():
+        raise forms.ValidationError('Password must contain at least one special character.')
 
 
 class SignUpForm(UserCreationForm):
@@ -12,7 +33,9 @@ class SignUpForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Enter Username',
         'class': 'w-full p-3 rounded-xl'
-    }))
+    }),
+        validators = [validators.MaxLengthValidator(150)]
+    )
 
     email = forms.CharField(widget=forms.EmailInput(attrs={
         'placeholder': 'Enter Your Email',
@@ -22,7 +45,9 @@ class SignUpForm(UserCreationForm):
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Enter Password',
         'class': 'w-full p-3 rounded-xl'
-    }))
+    }),
+        validators = [password_validator]
+    )
 
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Repeat Password',
